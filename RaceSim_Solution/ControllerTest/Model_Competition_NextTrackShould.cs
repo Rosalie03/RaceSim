b861033867c94d78e31283e23e394e22bee69ed3
Rosalie03 <s1173877@student.windesheim.nl>
2022-09-21 12:18:57 +0200

    3 af?

--- RaceSim_Solution/ControllerTest/Model_Competition_NextTrackShould.cs
+++ RaceSim_Solution/ControllerTest/Model_Competition_NextTrackShould.cs
@@ -28,6 +28,79 @@ namespace ControllerTest
             Track result = _competition.NextTrack();
             Assert.IsNull(result);
         }
+
+        public void NextTrack_OneInQueue_ReturnTrack()
+        {
+            Track tr = new Track("R4", new[]
+            {
+                Section.SectionTypes.Startgrid,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Rightcorner,
+                Section.SectionTypes.Finish,
+            });
+
+            _competition.Tracks.Enqueue(tr);
+             Track result = _competition.NextTrack();
+
+            Assert.Equals(tr, result);
+        }
+
+        public void NextTrack_OneInQueue_RemoveTrackFromQueue()
+        {
+            Track tr2 = new Track("R5", new[]
+            {
+                Section.SectionTypes.Startgrid,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Finish,
+
+            });
+
+            _competition.Tracks.Enqueue(tr2);
+
+            Track result2 = _competition.NextTrack();   
+            result2 = _competition.NextTrack();  
+            
+            Assert.IsNull(result2);
+        }
+
+        public void NextTrack_TwoInQueue_ReturnNextTrack()
+        {
+            Track tr3 = new Track("R6", new[]
+            {
+                Section.SectionTypes.Startgrid,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Lefcorner,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Finish,
+
+            });
+
+        _competition.Tracks.Enqueue(tr3);
+
+            Track tr4 = new Track("R7", new[]
+{
+                Section.SectionTypes.Startgrid,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Rightcorner,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Straight,
+                Section.SectionTypes.Finish,
+
+            });
+
+            _competition.Tracks.Enqueue(tr4);
+
+            _competition.NextTrack();
+            Track result = _competition.NextTrack();
+
+            Assert.Equals(tr4, result);
+            
+        }
         #endregion
     }
 }
